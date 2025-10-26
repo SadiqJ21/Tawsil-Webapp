98dd0790d87c6f813eaf6781f6f1c4f1a891a9cb
SadiqJ21 <sadiqjaffermh@gmail.com>
2025-10-27 01:08:25 +0300

    modify 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/Tawsil-Webapp/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
